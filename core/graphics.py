aeeb69cf5b88173b52a6765f20e271b58bae417d
Connor Sutton <connorsutton07@gmail.com>
2021-03-23 23:55:27 -0500

    added ability to apply texture to shape, but not using atm

--- core/graphics.py
+++ core/graphics.py
@@ -23,8 +23,8 @@ def apply_alpha(texture, mask):
     del target
     return texture
 
-def stamp(image, texture, mask, pos):
-    image.blit(apply_alpha(texture, mask), pos)
+def stamp(screen, texture, mask, pos):
+    screen.blit(apply_alpha(texture, mask), pos)
 
 def blit_textured_shape(screen, texture_path: str, blit_pos: tuple, mask: pygame.Surface) -> None:
     texture = tile_texture(pygame.image.load(texture_path), mask.get_size())
