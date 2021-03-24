16a6a6cd149975ce25afd6a7c0aefa52a3a14e1b
gholmesss <g.holmes429@gmail.com>
2021-03-24 12:02:46 -0500

    Added untested racecar and neural network

---
+++ core/neural_net.py
@@ -0,0 +1,97 @@
+"""
+Contains basic feed forward neural network class.
+"""
+
+from numba import jit
+import numpy as np
+
+
+class FFNN:
+	"""
+	Feed forward neural network without backpropogation.
+
+	Public Methods
+	--------------
+	feedForward(a) -> np.ndarray:
+		Feeds inputs through neural net to obtain output.
+	"""
+	def __init__(self, layerSizes: list, activation: str = "sigmoid", outputActivation: str = "softmax", weights: list = None, biases: list = None) -> None:
+		"""
+		Initializes.
+
+		Parameters
+		----------
+		layerSizes: list
+			Layer architecture
+		activation: str, default="sigmoid"
+			String denoting activation function to use
+		outputActivation: str, default="softmax"
+			String denoting output layer activation function to use
+		weights: list, optional
+			List of arrays of weights for each layer, randomized if not passed in
+		biases: list, optional
+			List of arrays of biases for each layer, randomized if not passed in
+		"""
+		activations = {
+			"sigmoid": FFNN.sigmoid,
+			"reLu": FFNN.reLu,
+			"softmax": FFNN.softmax,
+			"linear": FFNN.linear
+		}
+		self.layerSizes = layerSizes
+		weightShapes = [(i, j) for i, j in zip(layerSizes[1:], layerSizes[:-1])]
+		self.weights = [np.random.randn(*s) for s in weightShapes] if weights is None else weights
+		self.biases = [np.random.standard_normal(s) for s in layerSizes[1:]] if biases is None else biases
+		self.activation = activations[activation]
+		self.outputActivation = activations[outputActivation]
+
+	def feedForward(self, a: np.ndarray) -> np.ndarray:
+		"""
+		Feeds inputs through neural net to obtain output.
+
+		Parameters
+		----------
+		a: np.ndarray
+			Inputs to neural network
+
+		Returns
+		-------
+		np.ndarray: input after fed through neural network
+		"""
+		for w, b in zip(self.weights[:-1], self.biases[:-1]):
+			a = self.activation(a @ w.T + b)
+		return self.activation(a @ self.weights[-1].T + self.biases[-1])
+
+	def get_params(self) -> dict:
+		return {
+			"weights": self.weights,
+			"biases": self.biases,
+			"architecture": self.layerSizes,
+			"activation": self.activation.__name__,
+			"output activation": self.outputActivation.__name__,
+		}
+
+	@staticmethod
+	@jit(nopython=True)
+	def sigmoid(x: float) -> float:
+		"""Sigmoid."""
+		return 1 / (1 + np.exp(-x))
+
+	@staticmethod
+	@jit(nopython=True)
+	def reLu(x: float) -> float:
+		"""Rectified linear unit."""
+		return np.maximum(0, x)
+
+	@staticmethod
+	@jit(nopython=True)
+	def softmax(v: np.ndarray) -> np.ndarray:
+		"""Softmax probability output."""
+		e = np.exp(v)
+		return e / e.sum()	
+
+	@staticmethod
+	def linear(x: float) -> float:
+		"""Linear activation"""
+		return x
+	
---
+++ core/racecar.py
@@ -0,0 +1,76 @@
+"""
+Race car with neural network decision making.
+"""
+
+import numpy as np
+
+from neural_net import FFNN
+
+
+class Racecar:
+	"""Haven't tested any of this yet."""
+	def __init__(
+				self,
+				id: str,
+				architecture: tuple = (8, 6, 3),
+				initial_pos: np.ndarray = None,
+				initial_vel: np.ndarray = None,
+				initial_accel: np.ndarray = None
+				) -> None:
+		self.id = id  # this should be base 36 number for uniqueness and to minimize digits
+		self.network = FFNN(architecture, outputAcivation = "linear")  # can decide turning
+		self.initial_p = initial_pos if initial_pos is not None else np.array([0., 0.])  # position
+		self.initial_v = initial_vel if initial_vel is not None else np.array([0., 0.])  # velocity
+		self.initial_a = initial_accel if initial_accel is not None else np.array([0., 0.])  # acceleration
+		self.p = self.initial_p.copy()
+		self.v = self.initial_v.copy()
+		self.a = self.initial_a.copy()
+		self.steps = 0  # number of steps made
+		self.alive = True
+		self.resets = 0
+
+	def step(self) -> None:
+		"""Updates cars state."""
+		self.p += self.v
+		self.v *= self.a
+		self.steps += 1
+
+	def steer(self, theta: float) -> None:
+		"""Rotates direction ccw by theta degrees in radians, def need to test this."""
+		self.v = np.array([[np.cos(theta), -sin(theta)], [sin(theta), cos(theta)]]) @ self.v
+
+	def get_optimal_state(self, rayLengths: np.ndarray) -> float:
+		"""Gets turn angle from neural network."""
+		return self.network.feedForward(rayLengths)  # turn angle, accel
+
+	def accelerate(self, a) -> None:
+		"""Sets acceleration."""
+		self.a = a
+
+	def is_alive(self) -> bool:
+		"""Returns whether racecar is alive or not."""
+		return self.alive
+
+	def kill(self) -> None:
+		"""Kills racecar and sets pos and vel to 0."""
+		self.alive = False
+		self.p = np.array([0., 0.])
+		self.v = np.array([0., 0.])
+
+	def get_network_params(self) -> dict:
+		"""Gets important params from network."""
+		return self.network.getParams()
+
+	def reset(self) -> None:
+ 		"""Resets state of this racecar to initial values."""
+		self.p = self.initial_p.copy()
+		self.v = self.initial_v.copy()
+		self.a = self.initial_a.copy()
+		self.steps = 0  # number of steps made
+		self.alive = True
+		self.resets += 1
+
+	def __eq__(self, other: Racecar) -> bool:
+		"""Checks if this racecar and other racecar are equal via id."""
+		return self.id == other.id
+
