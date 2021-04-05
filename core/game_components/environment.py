19faefc6d794766b8c8659eae25439a273869236
Connor Sutton <connorsutton07@gmail.com>
2021-04-05 13:35:44 -0500

    added documentation

--- core/game_components/environment.py
+++ core/game_components/environment.py
@@ -12,6 +12,11 @@ class Environment:
 
 
     def prepareTrack(self) -> None:
+        """ 
+        Scales the track to the proper size and moves it to
+        the center of the map based on variables in Settings
+
+        """
         inner_edges = self.track.getInnerEdges()
         outer_edges = self.track.getOuterEdges()
         inner_edges = list(map(lambda x: (x[0] * TRACK_SCALE + TRACK_ORIGIN[0], x[1] * TRACK_SCALE + TRACK_ORIGIN[1]), inner_edges))
