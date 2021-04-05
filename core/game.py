7b416d9553de13cc333a36af76d400ba4cc6a32c
Connor Sutton <connorsutton07@gmail.com>
2021-04-05 13:30:58 -0500

    offloaded work for creating track surface to single-use function

--- core/game.py
+++ core/game.py
@@ -9,44 +9,57 @@ from core.settings import *
 from core.ui.engine import Engine
 import keyboard
 
+def prepareTrackSurface(engine: Engine, environment: Environment) -> Engine.Surface:
+    track = environment.track
+
+    # retrieves the coordinates for all points on the inside and outside edges of the track
+    inner_edges = track.getInnerEdges()
+    outer_edges = track.getOuterEdges()
+
+    # create empty surfaces that will contain the inside/outside edges
+    outer_surface = engine.Surface(SCREEN_SIZE, flag="srcalpha")
+    inner_surface = engine.Surface(SCREEN_SIZE, flag="srcalpha")
+
+    # draw the edges to the corresponding surfaces
+    engine.renderPolygon((255, 255, 255), outer_edges, outer_surface)
+    engine.renderPolygon((0, 0, 0), inner_edges, inner_surface)
+
+    # load the texture that will be applied to the track
+    texture = engine.load_image("texture2.png")
+    texture = engine.tile_surface(texture)
+
+    # remove the inside surface from the outside to create the track surface and apply the texture to the result
+    outer_surface = (outer_surface - inner_surface).apply_texture(texture)
+
+    return outer_surface
+
+
+
 def PvAI():
     track = Track(type=TRACK_TYPE)
     engine = Engine(SCREEN_SIZE, (1, 1), checkered=False, imageFolder=os.path.join(os.getcwd(), "assets"))
     environment = Environment(track)
+    track_surface = prepareTrackSurface(engine, environment)
 
     while not keyboard.is_pressed('esc'):
         for step in range(1, SMOOTHNESS + 1):
             if engine.shouldRun():
                 engine.clearScreen()
-                engine.renderScene(_renderEnvironment, environment)
+                engine.renderScene(_renderEnvironment, environment, track_surface)
                 engine.updateScreen()
 
     engine.exit()
 
 
     
-def _renderEnvironment(engine: Engine, environment: Environment):
-    track = environment.track
+def _renderEnvironment(engine: Engine, environment: Environment, track_surface: Engine.Surface) -> None:
+    """ 
+    Contains all the instructions for the engine to render 
+    the environment to the screen
 
+    """
     engine.tileImageAsBackground("grass.png")
-    #texture = engine.Image("texture2.png")
-    texture = engine.load_image("texture2.png")
-    texture = engine.tile_surface(texture)
-    inner_edges = track.getInnerEdges()
-    outer_edges = track.getOuterEdges()
-
-    inner_edges = list(map(lambda x: (x[0] * TRACK_SCALE + TRACK_ORIGIN[0], x[1] * TRACK_SCALE + TRACK_ORIGIN[1]), inner_edges))
-    outer_edges = list(map(lambda x: (x[0] * TRACK_SCALE + TRACK_ORIGIN[0], x[1] * TRACK_SCALE + TRACK_ORIGIN[1]), outer_edges))
-
-    outer_surface = engine.Surface(SCREEN_SIZE, flag="srcalpha")
-    inner_surface = engine.Surface(SCREEN_SIZE, flag="srcalpha")
-
-
-    engine.renderPolygon((255, 255, 255), outer_edges, outer_surface)
-    engine.renderPolygon((0, 0, 0), inner_edges, inner_surface)
-
-    outer_surface = (outer_surface - inner_surface).apply_texture(texture)
-    engine.renderSurface(outer_surface.surface)
+    engine.renderSurface(track_surface)
 
 
 
